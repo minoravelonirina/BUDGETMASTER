e11becf7ed26abab04914bdb166405b4be901e36
minosoa <minoravelonirina@gmail.com>
2024-08-21 10:01:10 +0300

    feat: add all method for user

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,19 +4,40 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 @Setter
 @AllArgsConstructor
 
 public class User {
-    private String lastname;
-    private String firstname;
-    private ProfilUser profilUser;
-    private LocalDate birthday;
-    private String address;
-    private String contact;
-    private String email;
+    private String userName;
+    private double BudgetMensual;
+    private List<Depence> depenes;
 
-    
+
+    public void addExpense(Depence newDepence){
+        getDepenes().add(newDepence);
+    }
+
+    public List<Depence> getExpenseByCategory(String categorie){
+        getDepenes()
+                .stream()
+                .filter(e -> !categorie.equals(e.getCategorie()));
+        return List.of();
+    }
+
+    public double getTotalSpentThisMonth(){
+        double acc = 0.0;
+        LocalDate currentDate = LocalDate.now();
+        int month = currentDate.getMonthValue();
+
+        for (Depence depene : getDepenes()) {
+            if (depene.getDate().getMonthValue() == month){
+                acc += depene.getMontant();
+            }
+        }
+        return acc;
+    }
 }
